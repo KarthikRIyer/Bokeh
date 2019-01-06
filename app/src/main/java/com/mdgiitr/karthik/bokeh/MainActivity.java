7ee90b71abdc7171620259d2b16adcacaac74acf
jsuyash1514 <jsuyash1514@gmail.com>
2019-01-06 22:17:01 +0530

    Calculate pixel array for actual image

--- app/src/main/java/com/mdgiitr/karthik/bokeh/MainActivity.java
+++ app/src/main/java/com/mdgiitr/karthik/bokeh/MainActivity.java
@@ -1,10 +1,24 @@
 package com.mdgiitr.karthik.bokeh;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+    ImageView imageDisplay;
+    Button chooseImage;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -16,9 +30,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageDisplay = findViewById(R.id.imageview);
+        chooseImage = findViewById(R.id.button_choose_image);
+
+        chooseImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI), 100);
+            }
+        });
+
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+//        TextView tv = (TextView) findViewById(R.id.sample_text);
+//        tv.setText(stringFromJNI());
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100 && resultCode == RESULT_OK) {
+            Uri selectedImage = data.getData();
+            Bitmap bitmap;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                if (bitmap != null) {
+                    int w = bitmap.getWidth();
+                    int h = bitmap.getHeight();
+                    float[][] redActual = new float[h][w];
+                    float[][] greenActual = new float[h][w];
+                    float[][] blueActual = new float[h][w];
+                    imageDisplay.setImageBitmap(bitmap);
+                    for (int i =0;i<bitmap.getHeight();i++){
+                        for(int j=0;j<bitmap.getWidth();j++){
+                            redActual[i][j] = (float)Color.red(bitmap.getPixel(j,i));
+                            greenActual[i][j] = (float)Color.green(bitmap.getPixel(j,i));
+                            blueActual[i][j] = (float)Color.blue(bitmap.getPixel(j,i));
+                        }
+                    }
+
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     /**
